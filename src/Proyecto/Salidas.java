8381b57fd6eeb307b1339fa301988bde5096f5e7
lo397 <lo3977884@gmail.com>
2023-03-02 11:40:17 -0600

    Primera

---
+++ src/Proyecto/Salidas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto;
+
+/**
+ *
+ * @author lo397
+ */
+public class Salidas extends Barco{
+    private int Id_Salida;
+    private String Fecha;
+    private String HoraSalida;
+    private String Destino;
+    
+    
+}
